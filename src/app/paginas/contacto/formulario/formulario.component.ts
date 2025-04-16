484fdbbe201b1e7560ea310c2ba365fdf0db2279
darnok45 <parragenaro429@gmail.com>
2025-04-16 14:53:07 -0300

    creacion de paginas

--- src/app/paginas/contacto/formulario/formulario.component.ts
+++ src/app/paginas/contacto/formulario/formulario.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-formulario',
-  imports: [],
+  imports: [ReactiveFormsModule,FormsModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
