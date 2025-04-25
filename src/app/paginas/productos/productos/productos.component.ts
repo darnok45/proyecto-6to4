b4ca0e0f1d5ab004cdb6e20769cbcc6c073b4825
darnok45 <parragenaro429@gmail.com>
2025-04-25 10:44:18 -0300

    terminacion de servicios con productos

--- src/app/paginas/productos/productos/productos.component.ts
+++ src/app/paginas/productos/productos/productos.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Producto } from '../../../modelos/producto.model';
+import { CarritoService } from '../../../servicios/carrito.service';
 
 @Component({
   selector: 'app-productos',
@@ -7,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './productos.component.css'
 })
 export class ProductosComponent {
+  Productos: Producto[] = [
+    {
+      id:1,
+      nombre :'Hola',
+      descripcion:'lo mira feo',
+      precio : 2000,
+      imagen: '',
+      disponibilidad : true,
+    },
+  ]
+  constructor(private carritoService: CarritoService){}
+
+  // Metodo para agreagr un producto al carrito
+  agregar(producto: Producto){
+    this.carritoService.agregarAlCarrito(producto)
+    alert('Producto agregado al carrito') //Muestra el mensaje
+  }
 
 }
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CarritoService } from '../../servicios/carrito.service';
+import { Producto } from '../../modelos/producto.model';
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +9,21 @@ import { RouterLink } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  cantidadProductos: number = 0;
+
+  constructor(private carritoService: CarritoService){}
+
+  ngOnInit(): void {
+    // Escucha los cambios en el carrito para actualizar la cantidad total de productos
+
+    this.carritoService.carrito$.subscribe((productos: { producto: Producto,cantidad : number}[]) => {
+      this.cantidadProductos = productos.reduce((total, item) => total + item.cantidad,0) //Suma la cantidad de productos
+    })
+  }
+
+  onCarritoClick(){
+    console.log('Carrito clicked');
+  }
 
 }
